b718afb77dcd894112a9fa213b74747f8723a5d8
botmi25102002 <botmi25102002@gmail.com>
2022-03-23 03:41:32 +0700

    reset code

---
+++ baithuchanhngay21-2-2022/App35.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class App35 {
+ public static void main(String[] args){
+  int a, b; 
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Nhập vào số thứ nhất: ");
+      a = sc.nextInt();
+  System.out.println("Nhập vào số thứ hai: ");
+      b = sc.nextInt();
+   int minNumber = a;
+if (a > b) 
+      minNumber = b;
+  System.out.println("Số nhỏ nhất trong hai số " + a+ " và "+ b+ "là " + minNumber);
+    } 
+}
---
+++ baithuchanhngay21-2-2022/App47.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class App47 {
+
+  public static void main(String[] args) {
+
+    int n, sum = 0;
+
+    Scanner scanner = new Scanner(System.in);
+
+    do {
+
+      System.out.println("Nhập vào số nguyên bất kỳ: ");
+
+      n = scanner.nextInt();
+
+      sum += n;
+
+    } while (sum < 100);
+
+    System.out.println("Tổng các số nguyên vừa nhập = " + sum);
+
+  }
+}
